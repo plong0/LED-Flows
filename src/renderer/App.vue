022551851e39b42979aabb9cafd0e2badb22f7e0
plong0 <plong00@gmail.com>
2018-09-27 19:29:07 -0600

    Adds the text-wrap-none global style

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -159,4 +159,8 @@
 <style>
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons');
   /* Global CSS */
+  .text-wrap-none {
+    white-space: nowrap;
+    overflow: visible;
+  }
 </style>
