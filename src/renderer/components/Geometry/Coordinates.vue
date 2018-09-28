05bcbf54ed04045ce8dae80cb18fb943e92d1540
plong0 <plong00@gmail.com>
2018-09-27 19:29:48 -0600

    Adds the Coordinates component

---
+++ src/renderer/components/Geometry/Coordinates.vue
@@ -0,0 +1,39 @@
+<template>
+  <span class="text-wrap-none">
+    ( {{coordinates}} )
+  </span>
+</template>
+
+<script>
+  export default {
+    props: {
+      precision: {
+        type: Number,
+        default: 0
+      },
+      x: {
+        type: Number,
+        required: true
+      },
+      y: {
+        type: Number,
+        required: true
+      },
+      z: {
+        type: Number
+      }
+    },
+    computed: {
+      coordinates () {
+        return `${this.coordinate(this.x)} , ${this.coordinate(this.y)}` + (!isNaN(this.z) ? ` , ${this.coordinate(this.z)}` : '')
+      }
+    },
+    methods: {
+      coordinate (value) {
+        if (typeof value === 'number') {
+          return value.toFixed(this.precision)
+        }
+      }
+    }
+  }
+</script>
