fb659eed9d457844a0dff03accae711392e03b0c
plong0 <plong00@gmail.com>
2018-10-07 22:17:42 -0600

    Adds onKeyDown/Up to PaperLightTool baseEvents

--- src/renderer/bridge/PaperLights/Tools/PaperLightTool.js
+++ src/renderer/bridge/PaperLights/Tools/PaperLightTool.js
@@ -6,22 +6,33 @@ export default class PaperLightTool extends Tool {
     this.$PL = paperLights
     this.active = false
     this.params = {}
+    this.pressedKeys = []
     this.baseEvents = {
-      onMouseUp: (event) => {
-        if (event.event.button === 2) {
-          this.deactivate()
-        }
-      },
-      onKeyUp: (event) => {
-        if (event.key === 'escape') {
-          this.deactivate()
-        }
-      },
       onActivate: (event) => {
         this.active = true
       },
       onDeactivate: (event) => {
         this.active = false
+      },
+      onKeyDown: (event) => {
+        if (event.key && !this.pressedKeys.includes(event.key)) {
+          this.pressedKeys.push(event.key)
+        }
+      },
+      onKeyUp: (event) => {
+        const pressedIndex = (event.key ? this.pressedKeys.indexOf(event.key) : -1)
+        if (pressedIndex !== -1) {
+          this.pressedKeys.splice(pressedIndex, 1)
+        }
+        if (event.key === 'escape') {
+          this.deactivate()
+          return true
+        }
+      },
+      onMouseUp: (event) => {
+        if (event.event.button === 2) {
+          this.deactivate()
+        }
       }
     }
     this.validEventTypes = [
